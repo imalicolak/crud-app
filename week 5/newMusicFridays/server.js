ca7abf39754666e65eb9e562e9f230249ef6bd9a
Ali Colak <19acolak99@gmail.com>
2023-03-09 10:56:29 -0600

    Server almost running

---
+++ week 5/newMusicFridays/server.js
@@ -0,0 +1,7 @@
+// npm init -y -> mkdir controllers models views -> touch .gitignore .env
+
+// Step 1: Get the server running
+const express =  require('express');
+const app = express();
+
+const PORT = 3000;
