9f2c1ab2f6007233fa10f33a6eff530e9ca67624
Ali Colak <19acolak99@gmail.com>
2023-03-09 13:21:00 -0600

    Routes Set

--- week 5/newMusicFridays/controllers/music.js
+++ week 5/newMusicFridays/controllers/music.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router  = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/music', (req, res) => {
     res.send('<h1> My list of music </h1>')
 })
 
--- week 5/newMusicFridays/server.js
+++ week 5/newMusicFridays/server.js
@@ -10,6 +10,22 @@ const PORT = 4000;
 // Bringing in exports from music controller, simplifying file link for routing purposes. 
 const musicController = require('./controllers/music')
 
+// Set up View Engine to ejs
+app.set('view engine', 'ejs');
+
+// Set generic home route & other routes
+app.get('/', (req, res) =>{
+    res.render('index');
+});
+app.get('/submit', (req, res) => {
+    res.render('submit');
+});
+app.get('/*', (req, res) => {
+    res.render('error')
+});
+
+
+//Listening on PORT
 app.listen(PORT,  () =>{
     console.log('Server running on port ' + PORT);
 });
