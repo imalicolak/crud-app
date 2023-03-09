751d0e55c19520a21c35bd0b002aff1506d59199
Ali Colak <19acolak99@gmail.com>
2023-03-09 12:31:02 -0600

    Server up and running on PORT 4000

---
+++ week 5/newMusicFridays/controllers/music.js
@@ -0,0 +1,9 @@
+// Router is what we use in controllers to route -> easy to export all of the routes in the file
+const express = require('express');
+const router  = express.Router();
+
+router.get('/', (req, res) => {
+    res.send('<h1> My list of music </h1>')
+})
+
+module.exports = router;
--- week 5/newMusicFridays/server.js
+++ week 5/newMusicFridays/server.js
@@ -4,4 +4,12 @@
 const express =  require('express');
 const app = express();
 
-const PORT = 3000;
+// Set PORT number
+const PORT = 4000;
+
+// Bringing in exports from music controller, simplifying file link for routing purposes. 
+const musicController = require('./controllers/music')
+
+app.listen(PORT,  () =>{
+    console.log('Server running on port ' + PORT);
+});
