b60f6aa8a4b2e740c139fd9039c7ed1f637b8770
Ali Colak <19acolak99@gmail.com>
2023-03-10 00:03:34 -0600

    Added Schema

--- week 5/newMusicFridays/config/connection.js
+++ week 5/newMusicFridays/config/connection.js
@@ -2,5 +2,29 @@
 require('dotenv').config();
 
 // How you get PRIVATE CODE READ
-console.log(process.env.MONGO_DB_URI) 
+const connectionString = (process.env.MONGO_DB_URI) ;
+
+
+const mongoose = require('mongoose');
+
+//Name of created Mongo DataBase
+mongoose.connect(connectionString) 
+
+// Successful Connection
+mongoose.connection.on('connected', () => {
+    console.log(`[${new Date().toLocaleTimeString()}] - Mongoose connected to MongoDB`);
+});
+
+// MongoDB connection on Error
+mongoose.connection.on('error', (error) => {
+    console.log(`Mongoose connection error: ${error}`);
+});
+
+// Disconnecting from MongoDB
+mongoose.connection.on('disconnected', () => {
+    console.log(`[${new Date().toLocaleTimeString()}] - Mongoose disconnected from MongoDB`);
+});
+
+
+
 
---
+++ week 5/newMusicFridays/models/songs.js
@@ -0,0 +1,38 @@
+// What data do we need for a song? 
+
+// Song Name, Artist Name, Song Category, Date Added, Song Rating
+
+const mongoose = require('mongoose');
+
+// Schema Object for Database
+const songSubmitSchema = new mongoose.Schema(
+{
+    songName:{
+        type: String,
+        required: [true, "Please type in the Song's Name"]
+    }, 
+    artistName: {
+        type: String,
+        required: [true, "Please type in the Artist's Name"]
+    }, 
+    songCategory: {
+        type: String,
+        required: [true, "Please type in the Song's Category"]
+    }, 
+    dateAdded: {
+        type: String,
+        required: [true, "Please type in the Friday Released"]
+    }, 
+    songRating: {
+        type: number,
+        required: [true, "Please type in the Song's Rating"]
+    }, 
+},
+    {
+        timestamps: true
+    }
+);
+
+const songs = mongoose.model('songs', songSubmitSchema);
+
+module.exports = songs;
